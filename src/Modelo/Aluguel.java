b2f18704f397dbc3d71973ff7170abd04309557f
ItaloFaud <italofaud10@hotmail.com>
2018-10-25 15:46:48 -0200

    Imagens, classes (Pacote modelo) e JFrames (Pacote principal)

---
+++ src/Modelo/Aluguel.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Hoope
+ */
+public class Aluguel {
+    private int cod;
+    private int cod_dvd;
+    private int cod_cliente;
+    private String data_aluguel;
+    private String horatio;
+    private String data_devolucao;
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public int getCod_dvd() {
+        return cod_dvd;
+    }
+
+    public void setCod_dvd(int cod_dvd) {
+        this.cod_dvd = cod_dvd;
+    }
+
+    public int getCod_cliente() {
+        return cod_cliente;
+    }
+
+    public void setCod_cliente(int cod_cliente) {
+        this.cod_cliente = cod_cliente;
+    }
+
+    public String getData_aluguel() {
+        return data_aluguel;
+    }
+
+    public void setData_aluguel(String data_aluguel) {
+        this.data_aluguel = data_aluguel;
+    }
+
+    public String getHoratio() {
+        return horatio;
+    }
+
+    public void setHoratio(String horatio) {
+        this.horatio = horatio;
+    }
+
+    public String getData_devolucao() {
+        return data_devolucao;
+    }
+
+    public void setData_devolucao(String data_devolucao) {
+        this.data_devolucao = data_devolucao;
+    }
+    
+    
+}
---
+++ src/Modelo/Cliente.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Hoope
+ */
+public class Cliente {
+    private int codigo;
+    private String nome;
+    private String nascimento;
+    private String rg;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private String bairro;
+    private String rua;
+    private int numero;
+    private String cep;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    
+    
+}
